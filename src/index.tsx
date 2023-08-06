8e5a2241bb73b7fa9612444ba0b5adb9c816c17a
HUMBEF0OL <123amitrana0123@gmail.com>
2023-08-06 12:44:49 +0530

    handled the logic for transpiling the text

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import * as esbuild from 'esbuild-wasm';
 
 const App = () => {
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
+    const ref = useRef<any>();
 
     useEffect(() => {
         startService();
     }, []);
     // initializing esbuild
     const startService = async () => {
-        const service = await esbuild.startService({
+        // after calling startService function we can refer to ref.current anywhere inside our component.
+        // reference to the service
+        ref.current = await esbuild.startService({
             worker: true,
             wasmURL: '/esbuild.wasm'
         });
-        console.log(service);
+
     }
-    const onClick = () => {
-        console.log(input)
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+        // for transpiling the text
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        });
+        // saving the transpiled text into code
+        setCode(result?.code);
     }
     return (<div>
         <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
