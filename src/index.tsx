1cedb82db6dafcbed752b603a65501c0770fbbf2
HUMBEF0OL <123amitrana0123@gmail.com>
2023-08-13 15:23:47 +0530

    handled the cases of different import statements

--- src/index.tsx
+++ src/index.tsx
@@ -30,7 +30,11 @@ const App = () => {
             entryPoints: ['index.js'],
             bundle: true,
             write: false,
-            plugins: [unpkgPathPlugin()]
+            plugins: [unpkgPathPlugin()],
+            define: {
+                'process.env.NODE_ENV': '"production"',
+                global: 'window'
+            }
         })
         // console.log(result)
         // saving the transpiled text into code
