e2da4ffb5d92d2c852ff703322fea0a66b9bd2ae
HUMBEF0OL <123amitrana0123@gmail.com>
2023-08-19 14:45:14 +0530

    revampmed the build.onResolve function in unpkg-path-plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -7,23 +7,24 @@ const fileCache = localforage.createInstance({
 });
 
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
     return {
         name: 'unpkg-path-plugin',
         setup(build: esbuild.PluginBuild) {
+            // handle root entry file of 'index.js'
+            build.onResolve({ filter: /(^index\.js$)/ }, () => {
+                return { path: 'index.js', namespace: 'a' };
+
+            })
+            // handle relative paths in a module
+            build.onResolve({ filter: /^\.+\// }, (args: any) => {
+                return {
+                    namespace: 'a',
+                    path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href,
+                };
+            })
+            // handle main file of module
             build.onResolve({ filter: /.*/ }, async (args: any) => {
-                console.log('onResolve', args);
-                if (args.path === 'index.js') {
-                    return { path: args.path, namespace: 'a' };
-                }
-
-                if (args.path.includes('./') || args.path.includes('../')) {
-                    return {
-                        namespace: 'a',
-                        path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href,
-                    };
-                }
-
                 return {
                     namespace: 'a',
                     path: `https://unpkg.com/${args.path}`,
@@ -37,11 +38,7 @@ export const unpkgPathPlugin = () => {
                 if (args.path === 'index.js') {
                     return {
                         loader: 'jsx',
-                        contents: `
-              import React from 'react';
-              const reactDOM = require('react-dom');
-              console.log(react,reactDOM);
-            `,
+                        contents: inputCode,
                     };
                 }
 
