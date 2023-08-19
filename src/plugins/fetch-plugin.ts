38ab9186e38dcb176004b784309ec1df14bcd057
HUMBEF0OL <123amitrana0123@gmail.com>
2023-08-19 15:28:54 +0530

    handled the css file issue

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -25,10 +25,19 @@ export const fetchPlugin = (inputCode: string) => {
                     return cachedResult;
                 }
                 const { data, request } = await axios.get(args.path);
-                console.log(request)
+
+                const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+                const escaped = data.replace(/\n/g, "").replace(/"/g, '\\"').replace(/'/g, "\\'");
+                const contents =
+                    fileType === 'css' ? `
+                        const style = document.createElement('style');
+                        style.innerText = '${escaped}';
+                        document.head.appendChild(style);
+                    ` : data;
+
                 const result: esbuild.OnLoadResult = {
                     loader: 'jsx',
-                    contents: data,
+                    contents,
                     resolveDir: new URL('./', request.responseURL).pathname
                 };
                 await fileCache.setItem(args.path, result);
