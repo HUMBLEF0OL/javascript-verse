0e8c106aa8c27633c9e61efce75bca8a70f0086e
HUMBEF0OL <123amitrana0123@gmail.com>
2023-08-30 12:13:11 +0530

    added the prettier package for formating of code

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,5 +1,8 @@
 // EditorDidMount is the type definition not the actual function
-import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
+import { useRef } from 'react';
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
 // https://blog.logrocket.com/build-web-editor-with-react-monaco-editor/#rewiring-react-app-work-with-monaco-editor
 // const options = {
@@ -29,35 +32,59 @@ interface CodeEditorProps {
     onChange(value: string): void;
 }
 
-const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+    const editorRef = useRef<any>();
+
     const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+        editorRef.current = monacoEditor;
         // adding event listener on the monacoEditor
         monacoEditor.onDidChangeModelContent(() => {
             onChange(getValue());
-            console.log(getValue())
         });
 
-        // for altering the tabSze of the editor
         monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
-    }
-    return <
-        MonacoEditor
-        editorDidMount={onEditorDidMount}
-        value={`${initialValue}\n`}
-        height='60vh'
-        theme='dark'
-        language="javascript"
-        options={{
-            wordWrap: 'on',
-            cursorBlinking: "expand",
-            minimap: { enabled: false },
-            showUnused: false,
-            folding: false,
-            lineNumbersMinChars: 3,
-            fontSize: 14,
-            scrollBeyondLastLine: false,
-            automaticLayout: true
-        }} />
+    };
+
+    const onFormatClick = () => {
+        // get current value from editor
+        const unformatted = editorRef.current.getModel().getValue();
+
+        // format that value
+        const formatted = prettier.format(unformatted, {
+            parser: 'babel',
+            plugins: [parser],
+            useTabs: false,
+            semi: true,
+            singleQuote: true,
+        });
+
+        // set the formatted value back in the editor
+        editorRef.current.setValue(formatted);
+    };
+
+    return (
+        <div>
+            <button onClick={onFormatClick}>Format</button>
+            <MonacoEditor
+                editorDidMount={onEditorDidMount}
+                value={initialValue}
+                theme="dark"
+                language="javascript"
+                height="500px"
+                options={{
+                    wordWrap: 'on',
+                    cursorBlinking: "expand",
+                    minimap: { enabled: false },
+                    showUnused: false,
+                    folding: false,
+                    lineNumbersMinChars: 3,
+                    fontSize: 14,
+                    scrollBeyondLastLine: false,
+                    automaticLayout: true
+                }}
+            />
+        </div>
+    );
 };
 
 export default CodeEditor;
