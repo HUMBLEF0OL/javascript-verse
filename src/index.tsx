f376e77e037324fc07efcff87f7fd1d558c8db4b
HUMBEF0OL <123amitrana0123@gmail.com>
2023-09-02 17:45:37 +0530

    separated the bundler logic from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,54 +1,21 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
 import Preview from './components/preview';
+import bundle from './bundler'
 
 const App = () => {
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
-    const ref = useRef<any>();
 
-    useEffect(() => {
-        startService();
-    }, []);
     // initializing esbuild
-    const startService = async () => {
-        // after calling startService function we can refer to ref.current anywhere inside our component.
-        // reference to the service
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-        });
 
-    }
     const onClick = async () => {
-        if (!ref.current) {
-            return;
-        }
-
-        // for transpiling the text
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [
-                unpkgPathPlugin(),
-                fetchPlugin(input)
-            ],
-            define: {
-                'process.env.NODE_ENV': '"production"',
-                global: 'window'
-            }
-        })
-        // console.log(result)
-        setCode(result?.outputFiles[0]?.text);
+        const output = await bundle(input);
+        setCode(output);
 
     }
-
     return (<div>
         <CodeEditor
             initialValue='/* Happy Coding */'
