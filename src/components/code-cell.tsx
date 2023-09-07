5ed5ce081745a911cfa575a1f112d926fd645014
HUMBEF0OL <123amitrana0123@gmail.com>
2023-09-07 17:54:59 +0530

    performace improvement for resizable component

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,22 +1,22 @@
 import { useState } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
-import bundle from '../bundler'
+import bundle from '../bundler';
 import Resizable from './resizable';
 
 const CodeCell = () => {
-    const [input, setInput] = useState('');
     const [code, setCode] = useState('');
+    const [input, setInput] = useState('');
 
     const onClick = async () => {
         const output = await bundle(input);
         setCode(output);
+    };
 
-    }
     return (
-        <Resizable direction='vertical'>
+        <Resizable direction="vertical">
             <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
-                <Resizable direction='horizontal'>
+                <Resizable direction="horizontal">
                     <CodeEditor
                         initialValue='/* Happy Coding */'
                         onChange={(value) => setInput(value)}
@@ -24,6 +24,8 @@ const CodeCell = () => {
                 </Resizable>
                 <Preview code={code} />
             </div>
-        </Resizable>);
-}
+        </Resizable>
+    );
+};
+
 export default CodeCell;
--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,5 @@
 import './resizable.css';
+import { useEffect, useState } from 'react';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
@@ -7,20 +8,49 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
     let resizableProps: ResizableBoxProps;
+    const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+    const [width, setWidth] = useState(window.innerWidth * 0.75)
+
+
+    useEffect(() => {
+        let timer: any;
+        const listener = () => {
+            if (timer) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(() => {
+
+            }, 50);
+            setInnerHeight(window.innerHeight);
+            setInnerWidth(window.innerWidth);
+            if (window.innerWidth * 0.75 < width) {
+                setWidth(window.innerWidth * 0.75)
+            }
+        };
+        window.addEventListener('resize', listener);
+
+        return () => {
+            window.removeEventListener('resize', listener);
+        }
+    }, [])
 
     if (direction === 'horizontal') {
         resizableProps = {
             className: 'resize-horizontal',
-            minConstraints: [window.innerWidth * 0.2, Infinity],
-            maxConstraints: [window.innerWidth * 0.75, Infinity],
+            minConstraints: [innerWidth * 0.2, Infinity],
+            maxConstraints: [innerWidth * 0.75, Infinity],
             height: Infinity,
-            width: window.innerWidth * 0.75,
+            width: width,
             resizeHandles: ['e'],
+            onResizeStop: (event, data) => {
+                setWidth(data.size.width);
+            }
         };
     } else {
         resizableProps = {
             minConstraints: [Infinity, 24],
-            maxConstraints: [Infinity, window.innerHeight * 0.9],
+            maxConstraints: [Infinity, innerHeight * 0.9],
             height: 300,
             width: Infinity,
             resizeHandles: ['s'],
