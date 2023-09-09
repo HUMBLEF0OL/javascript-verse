893c224dfb5a7827ff9987252ec14a0e8a0d87dd
HUMBEF0OL <123amitrana0123@gmail.com>
2023-09-09 10:35:29 +0530

    added auto bundle using useEffect

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import bundle from '../bundler';
@@ -8,10 +8,16 @@ const CodeCell = () => {
     const [code, setCode] = useState('');
     const [input, setInput] = useState('');
 
-    const onClick = async () => {
-        const output = await bundle(input);
-        setCode(output);
-    };
+    useEffect(() => {
+        const timer = setTimeout(async () => {
+            const output = await bundle(input);
+            setCode(output);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [input])
 
     return (
         <Resizable direction="vertical">
