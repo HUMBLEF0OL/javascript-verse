0275b8d265f2f965ad95152844fd609606870751
HUMBEF0OL <123amitrana0123@gmail.com>
2023-09-30 09:59:48 +0530

    added action-bar styling

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -2,6 +2,8 @@ import { Cell } from "../state";
 import ActionBar from "./action-bar";
 import CodeCell from "./code-cell";
 import TextEditor from "./text-editor";
+import './cell-list-item.css';
+
 
 interface CellListItemProps {
     cell: Cell
@@ -10,14 +12,21 @@ interface CellListItemProps {
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
     let child: JSX.Element;
     if (cell.type === 'code') {
-        child = <CodeCell cell={cell} />
+        child = <>
+            <div className="action-bar-wrapper">
+                <ActionBar id={cell.id} />
+            </div>
+            <CodeCell cell={cell} />
+        </>
     } else {
-        child = <TextEditor cell={cell} />
+        child = <>
+            <TextEditor cell={cell} />
+            <ActionBar id={cell.id} />
+        </>
     }
 
     return (
-        <div>
-            <ActionBar id={cell.id} />
+        <div className="cell-list-item">
             {child}
         </div>)
 }
