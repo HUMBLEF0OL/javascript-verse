7730369695affde9d82984143aea7cdd3b04c016
HUMBEF0OL <123amitrana0123@gmail.com>
2023-10-01 08:43:47 +0530

    added the styling for add-cell buttons

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -10,8 +10,20 @@ const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
 
     return (
         <div className='add-cell'>
-            <button onClick={() => insertCellBefore(nextCellId, 'code')}>Code</button>
-            <button onClick={() => insertCellBefore(nextCellId, 'text')}>Text</button>
+            <div className='add-buttons'>
+                <button className='button is-rounded is-primary is-small' onClick={() => insertCellBefore(nextCellId, 'text')}>
+                    <span className='icon is-small'>
+                        <i className='fas fa-plus' />
+                    </span>
+                    <span>Text</span>
+                </button>
+                <button className='button is-rounded is-primary is-small' onClick={() => insertCellBefore(nextCellId, 'code')}>
+                    <span className='icon is-small'>
+                        <i className='fas fa-plus' />
+                    </span>
+                    <span>Code</span>
+                </button>
+            </div>
             <div className='divider'></div>
         </div>
     )
