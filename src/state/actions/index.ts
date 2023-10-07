4873f3b02924feaa485b1103539a0ba0fe0160cf
HUMBEF0OL <123amitrana0123@gmail.com>
2023-10-07 09:43:59 +0530

    created the actionTypes for bundleReducer

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -33,4 +33,22 @@ export interface UpdateCellAction {
 
 export type direction = 'up' | 'down';
 
-export type Action = MoveCellAction | DeleteCellAction | InsertCellAfterAction | UpdateCellAction;
+export interface BundleStartAction {
+    type: ActionType.BUNDLE_START,
+    payload: {
+        cellId: string
+    }
+}
+
+export interface BundleCompleteAction {
+    type: ActionType.BUNDLE_COMPLETE,
+    payload: {
+        cellId: string,
+        bundle: {
+            code: string,
+            err: string
+        }
+    }
+}
+
+export type Action = MoveCellAction | DeleteCellAction | InsertCellAfterAction | UpdateCellAction | BundleStartAction | BundleCompleteAction;
