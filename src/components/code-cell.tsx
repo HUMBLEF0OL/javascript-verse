de6e35769f8074af7bf917f016920e31f45c6ae5
HUMBEF0OL <123amitrana0123@gmail.com>
2023-10-07 10:29:15 +0530

    removed the warnings from code-cell component

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -22,7 +22,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
         return () => {
             clearTimeout(timer);
         }
-    }, [cell.content, cell.id])
+    }, [cell.content, cell.id, createBundle])
 
     return (
         <Resizable direction="vertical">
--- src/hooks/use-actions.ts
+++ src/hooks/use-actions.ts
@@ -1,9 +1,12 @@
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../state";
+import { useMemo } from 'react'
 
 export const useActions = () => {
     const dispatch = useDispatch();
 
-    return bindActionCreators(actionCreators, dispatch);
+    return useMemo(() => {
+        return bindActionCreators(actionCreators, dispatch);
+    }, [dispatch])
 }
