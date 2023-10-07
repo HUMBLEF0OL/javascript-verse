ae47c5975b1b92c7d837f8db393819d31949a8e6
HUMBEF0OL <123amitrana0123@gmail.com>
2023-10-07 12:15:32 +0530

    implemented the progressBar

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -5,6 +5,7 @@ import Resizable from './resizable';
 import { Cell } from '../state';
 import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/used-typed-selector';
+import './code-cell.css'
 
 
 interface CodeCellProps {
@@ -38,7 +39,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
                         onChange={(value) => updateCell(cell.id, value)}
                     />
                 </Resizable>
-                {bundle && <Preview code={bundle.code} err={bundle.err} />}
+                <div className='progress-wrapper'>
+                    {
+                        !bundle || bundle.loading ?
+                            (
+                                <div className='progress-cover'>
+                                    <progress className='progress is-small is-primary' max={100}>Loading...</progress>
+                                </div>
+                            ) :
+                            (<Preview code={bundle.code} err={bundle.err} />)
+                    }
+                </div>
             </div>
         </Resizable>
     );
