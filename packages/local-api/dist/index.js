4caa2981f38e312b479ff8cdee50d9fc2ae8cff8
HUMBEF0OL <123amitrana0123@gmail.com>
2023-10-28 12:08:11 +0530

    wrapup

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,8 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
+const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
 const serve = (port, filename, dir) => {
-    console.log('serving traffic on port', port);
-    console.log(filename, dir);
+    const app = (0, express_1.default)();
+    app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+        target: 'http://127.0.0.1:3000',
+        ws: true,
+        logLevel: 'silent'
+    }));
+    return new Promise((resolve, reject) => {
+        app.listen(port, resolve).on('error', reject);
+    });
 };
 exports.serve = serve;
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export const serve = (port: number, filename: string, dir: string) => {
     const app = express();
-
+    app.use(createProxyMiddleware({
+        target: 'http://127.0.0.1:3000',
+        ws: true,
+        logLevel: 'silent'
+    }))
     return new Promise<void>((resolve, reject) => {
         app.listen(port, resolve).on('error', reject);
     });
